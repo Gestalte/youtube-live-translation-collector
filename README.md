e0fc7833848d2873b1c7b60d2b896302ed9ca2c1
Gestalte <philipbenade@gmail.com>
2021-06-12 20:23:33 +0200

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # youtube-live-translation-collector
 Creates a window in YouTube's the live chat window that shows live translations and spanner comments.
+
+# What this is for?
+Youtube Special Comment Sticker is a script for the [Tampermonkey Add-on](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) that adds a window to Youtube live stream chat that keeps messages left by the channel owner or moderators displayed.
+
+This edit also adds chat messages left by people doing live translations to this window.
+
+# How are live translations identified?
+Currently I do a case insensitive check for messages that start with the following:
+
+| EN | translation | tr | 英訳 | 英訳/EN
+| -- | ----------- | -- | ------ | --- | -- | -----
+| `[EN]` | `[translation]` | `[tr]` | `[英訳]` | `[英訳/EN]`
+| `EN:` | `translation:` | `tr:` | `英訳:` | `英訳/EN:`
+| `EN :` | `translation :` | `tr :` | `英訳 :` | `英訳/EN :`
+| `EN-` | `translation-` | `tr-` | `英訳-` | `英訳/EN-`
+| `EN -` | `translation -` | `tr -` | `英訳 -` | `英訳/EN -`
+| `EN}` | `translation}` | `tr}` | `英訳}` | `英訳/EN}`
+| `EN }` | `translation }` | `tr }` | `英訳 }` | `英訳/EN }`
+| `/[EN]` | `/[translation]` | `/[tr]` | `/[英訳]` | `/[英訳/EN]`
+| `/EN:` | `/translation:` | `/tr:` | `/英訳:` | `/英訳/EN:`
+| `/EN :` | `/translation :` | `/tr :` | `/英訳 :` | `/英訳/EN :`
+| `/EN-` | `/translation-` | `/tr-` | `/英訳-` | `/英訳/EN-`
+| `/EN -` | `/translation -` | `/tr -` | `/英訳 -` | `/英訳/EN -`
+| `/EN}` | `/translation}` | `/tr}` | `/英訳}` | `/英訳/EN}`
+| `/EN }` | `/translation }` | `/tr }` | `/英訳 }` | `/英訳/EN }`
---
+++ youtube-live-translation-collector.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         YouTube Live Translation Collector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/live_chat*
+// @grant        none
+// ==/UserScript==
+
+var commentIdentifier = (function() {
+    var basePattern = "(^\\/?(§)(\\s?:|\\s?-|\\s?\\})\\s|^\\/?\\[(§)\\])";
+    var thingsToMatch = ["英訳", "英訳\/en", "en", "tr", "translation"];
+    var pattern = basePattern.replace(/§/g, thingsToMatch.join('|'));
+
+    return {
+        isTranslation: function (txt) {
+            return RegExp(pattern, 'i').test(txt)
+        },
+        isSpecial: function(author) {
+            return author.classList 
+                ? (author.classList.contains('moderator') || author.classList.contains('owner')) 
+                : author.content.author.classes.indexOf('moderator') > -1;
+        },
+        identify: function(node) {
+            let authorElement = node.querySelector('#author-name');
+            let messageElement = node.querySelector('#message');
+        
+            let message = messageElement.innerText.trim();
+
+            if(commentIdentifier.isSpecial(authorElement) || commentIdentifier.isTranslation(message)) {
+                monitor.stickComment(node);
+            }
+        }
+    }
+}());
+
+var monitor = (function(){
+    return {
+        init: function() {
+            let chatItems = document.querySelector('#items.style-scope.yt-live-chat-item-list-renderer');
+
+            // stick items that exist on load
+            chatItems.querySelectorAll('yt-live-chat-text-message-renderer').forEach(commentIdentifier.identify); 
+
+            // stick mutations
+            let observer = new MutationObserver((mutations) => {
+                mutations.forEach((mutation) => {
+                    mutation.addedNodes.forEach(commentIdentifier.identify);
+                });
+            });
+
+            // start the observer
+            observer.observe(chatItems, { childList: true });
+        },
+        stickComment: function(node) {
+            let sticky = document.getElementById('sticky');
+
+            sticky.appendChild(node);
+
+            let chat = document.querySelector('#chat');
+            
+            let itemScroller = chat.querySelector('#item-scroller');
+            itemScroller.scrollTop = itemScroller.scrollHeight;   
+            sticky.scrollTop = sticky.scrollHeight;
+        }
+    }
+}());
+
+(function(){    
+    let sticky = document.createElement('div');
+    let ticker = document.getElementById('ticker');
+
+    sticky.id = 'sticky';
+    sticky.style.maxHeight = '30%';
+    sticky.style.overflow = 'auto';
+    
+    ticker.parentNode.insertBefore(sticky, ticker);
+
+    window.top.addEventListener('load', monitor.init());
+})();
